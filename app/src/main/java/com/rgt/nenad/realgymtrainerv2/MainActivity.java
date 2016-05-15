c5f7a928d7f525228f01e46e4dcb7a2679fb1c70
NenadStojanovic <nenad.stojanovic@gmx.com>
2016-05-15 21:23:59 +0200

    Gotova MainActivity

--- app/src/main/java/com/rgt/nenad/realgymtrainerv2/MainActivity.java
+++ app/src/main/java/com/rgt/nenad/realgymtrainerv2/MainActivity.java
@@ -1,26 +1,23 @@
 package com.rgt.nenad.realgymtrainerv2;
 
-import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     String lineIWant;
-    public String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "motivational_quotes.txt";
+    String nameIWant;
+    int LineCount = 23;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,30 +26,32 @@ public class MainActivity extends AppCompatActivity {
         //Hiding ActionBar
         getSupportActionBar().hide();
 
-        //Setting text into Quotes TextViews
+        //Setting text and font into Quotes TextViews
         Typeface tf = Typeface.createFromAsset(getAssets(),
                 "Fonts/veteran_typewriter.ttf");
         TextView tv = (TextView) findViewById(R.id.textViewQuotes);
         tv.setTypeface(tf);
-        char[] lineWant;
+
 
 
         TextView tv2 = (TextView) findViewById(R.id.textViewNames);
         tv2.setTypeface(tf);
-        tv2.setText("...Arnold Schwarzenegger");
 
-
-        AssetManager assetManager = getAssets();
-
-
+        //selecting random quotes
+        int RandIndex=(int)Math.ceil(Math.random()*LineCount);
+        if(RandIndex % 2 != 0)
+        {
+            RandIndex++;
+        }
 
         try {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("motivational_quotes.txt")));
-            for(int i = 0; i < 3; ++i)
+            for(int i = 0; i < RandIndex; ++i)
                 br.readLine();
 
-            String lineIWant = br.readLine();
+             lineIWant = br.readLine();
+            nameIWant = br.readLine();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -60,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         tv.setText(lineIWant);
+        tv2.setText(nameIWant);
 
 
     }
