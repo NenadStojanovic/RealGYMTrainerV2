a661e7f260ece57871ede1081b2b1e9af86e19e9
NenadStojanovic <nenad.stojanovic@gmx.com>
2016-05-15 19:07:31 +0200

    Promena 2

--- app/src/main/java/com/rgt/nenad/realgymtrainerv2/MainActivity.java
+++ app/src/main/java/com/rgt/nenad/realgymtrainerv2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rgt.nenad.realgymtrainerv2;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
 
         //Hiding ActionBar
         getSupportActionBar().hide();
+
+        //Setting text into Quotes TextView
+        Typeface tf = Typeface.createFromAsset(getAssets(),
+                "Fonts/veteran_typewriter.ttf");
+        TextView tv = (TextView) findViewById(R.id.textViewQuotes);
+        tv.setTypeface(tf);
+        tv.setText("It's simple. If it jiggles, it's \nFAT! \n   ...Arnold Schwarzenegger");
+
+
     }
 }
