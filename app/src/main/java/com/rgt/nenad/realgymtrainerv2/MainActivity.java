af51d8f8fa93154dab200cd7353d0f9f65bf036d
NenadStojanovic <nenad.stojanovic@gmx.com>
2016-05-15 18:34:35 +0200

    Initial commit

---
+++ app/src/main/java/com/rgt/nenad/realgymtrainerv2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rgt.nenad.realgymtrainerv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Hiding ActionBar
+        getSupportActionBar().hide();
+    }
+}
