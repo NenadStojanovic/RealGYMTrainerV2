42d04e5704fb97e3cc490a2139a2272039125c05
SandraJeremenkovic <innarea@gmx.com>
2016-05-20 19:26:59 +0200

    Profil Trening osnovna expendable lista

--- app/src/main/java/com/rgt/nenad/realgymtrainerv2/TreningActivity.java
+++ app/src/main/java/com/rgt/nenad/realgymtrainerv2/TreningActivity.java
@@ -2,8 +2,18 @@ package com.rgt.nenad.realgymtrainerv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+import java.util.ArrayList;
 
 public class TreningActivity extends AppCompatActivity {
+
+    //override BackButtonTransition
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    private ExpandableListView mExpandableList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +24,31 @@ public class TreningActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
-    }
+        mExpandableList = (ExpandableListView) findViewById(R.id.expandable_list);
 
 
-    //override BackButtonTransition
-    public void onBackPressed() {
-        super.onBackPressed();
-        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        ArrayList<Parent> arrayParents = new ArrayList<Parent>();
+        ArrayList<String> arrayChildren = new ArrayList<String>();
+
+        //here we set the parents and the children
+        for (int i = 0; i < 5; i++) {
+            //for each "i" create a new Parent object to set the title and the children
+            Parent parent = new Parent();
+            parent.setTitle("Vezba " + i);
+
+            arrayChildren = new ArrayList<String>();
+            for (int j = 0; j < 1; j++) {
+                arrayChildren.add("Ovo je text za opis vezbe ");
+            }
+            parent.setArrayChildren(arrayChildren);
+
+            //in this array we add the Parent object. We will use the arrayParents at the setAdapter
+            arrayParents.add(parent);
+        }
+
+        //sets the adapter that provides data to the list.
+        mExpandableList.setAdapter(new MyCustomAdapter(TreningActivity.this, arrayParents));
+
     }
 }
+
