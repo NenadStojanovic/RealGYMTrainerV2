442083d4cb381202b80e10dfd069fae7e2de227c
SandraJeremenkovic <innarea@gmx.com>
2016-05-20 20:05:28 +0200

    Profil Trening osnovna expendable lista+ slikica+dugme

--- app/src/main/java/com/rgt/nenad/realgymtrainerv2/ProfilActivity.java
+++ app/src/main/java/com/rgt/nenad/realgymtrainerv2/ProfilActivity.java
@@ -25,7 +25,7 @@ public class ProfilActivity extends AppCompatActivity {
     TextView Noge;
 
 
-
+    //komentar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
