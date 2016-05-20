18963e68192cbe588d4a11129a6eeffbcdb72ff3
SandraJeremenkovic <innarea@gmx.com>
2016-05-20 22:30:21 +0200

    Profil Trening osnovna expendable lista+ slikica+dugme

--- app/src/main/java/com/rgt/nenad/realgymtrainerv2/TreningActivity.java
+++ app/src/main/java/com/rgt/nenad/realgymtrainerv2/TreningActivity.java
@@ -3,9 +3,25 @@ package com.rgt.nenad.realgymtrainerv2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+
+
+
 
 public class TreningActivity extends AppCompatActivity {
+
+    int LineCount1 = 5;
+    String Naziv;
+    String Opis;
 
     //override BackButtonTransition
     public void onBackPressed() {
@@ -28,17 +44,62 @@ public class TreningActivity extends AppCompatActivity {
 
 
         ArrayList<Parent> arrayParents = new ArrayList<Parent>();
-        ArrayList<String> arrayChildren = new ArrayList<String>();
+        ArrayList<Child> arrayChildren;
 
-        //here we set the parents and the children
-        for (int i = 0; i < 5; i++) {
+        //OVO CITA TXT DATOTEKE ZA NAZIV I OPIS
+        //PARENT
+
+        TextView papa = (TextView) findViewById(R.id.list_item_text_view);
+        TextView dete = (TextView) findViewById(R.id.list_item_text_child);
+
+        int duz=LineCount1;
+        int RandInd1;
+        int ind;
+
+        for (int i = 0; i < 3; i++) {
             //for each "i" create a new Parent object to set the title and the children
+            //Naziv
+            RandInd1=(int)Math.ceil(Math.random()*LineCount1);
             Parent parent = new Parent();
-            parent.setTitle("Naziv vezbe " + i);
+            try {
 
-            arrayChildren = new ArrayList<String>();
-            for (int j = 0; j < 1; j++) {
-                arrayChildren.add("Ovo je neki random text iz txt fajla ");
+                BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("grudi.txt")));
+                for(int j = 0; j < RandInd1; ++j) {
+                   Naziv=br.readLine();
+                }
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            parent.setTitle(i+"-"+Naziv);
+
+
+
+            //opis vezbi
+            arrayChildren = new ArrayList<Child>();
+            Child child=new Child();
+            for (int l = 0; l < 1; ++l) {
+                try {
+
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(getAssets().open("grudiOpisVezbi.txt")));
+                    for(int m = 0; m < RandInd1; m++) {
+                        br1.readLine();
+                        Opis= br1.readLine();
+                    }
+
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                child.setTitle(Opis);
+                arrayChildren.add(child);
+
             }
             parent.setArrayChildren(arrayChildren);
 
